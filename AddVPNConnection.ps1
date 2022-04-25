b544a4057efdaab274d5274b3b06763805578a62
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-04-25 11:31:19 +0200

    Update AddVPNConnection.ps1

--- AddVPNConnection.ps1
+++ AddVPNConnection.ps1
@@ -2,10 +2,11 @@ $VPNName = Read-Host -Prompt "Enter A Name Of The VPN Connection"
 $SeverAdress = Read-Host -Prompt "Enter IPAdress or Domainname to Server"
 $PreSharedKey = Read-Host -Prompt "Enter The PreSharedKey Password" -AsSecureString
 $username = Read-Host -Prompt "Enter Username"
-$plainpassword = Read-Host -Prompt "Enter Password" -AsSecureString
+$plainpassword = Read-Host -Prompt "Enter Password"
 
 #ADD VPN-CONNECTION (In Win 10 TO CONNECT TO SERVER)
-Add-VpnConnection -Name $VPNName -ServerAddress $SeverAdress -AuthenticationMethod MSChapv2 -EncryptionLevel Optional -Force -L2tpPsk $PreSharedKey -TunnelType L2tp -UseWinlogonCredential -RememberCredential -PassThru
+Write-Verbose "Adds A VPN-Connection" -Verbose
+Add-VpnConnection -Name $VPNName -ServerAddress $SeverAdress -AuthenticationMethod MSChapv2 -EncryptionLevel Optional -Force -L2tpPsk $PreSharedKey -TunnelType L2tp  -RememberCredential -PassThru
 
 #GET VPN-CONNECTION INFO AND DIAL UP AND THEN SEE STATUS
 Get-VpnConnection -Name $VPNName
